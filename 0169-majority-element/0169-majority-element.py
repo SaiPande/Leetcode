37c0dbc3e79540051631ad696c22e8104a542394
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-04-13 15:33:15 -0400

    Time: 10 ms (34.65%), Space: 19.4 MB (100%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        dict = {} #num: freq
+
+        for i in nums:
+            if i in dict:
+                dict[i] += 1
+
+            else: 
+                dict[i] = 1 
+
+        max_key = max(dict, key=dict.get)
+        return max_key      
