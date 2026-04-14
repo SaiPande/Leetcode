712dd9b90756c287d6ea03c9036109adec9a775d
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-04-14 10:30:59 -0400

    Time: 14 ms (27.12%), Space: 18.5 MB (100%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        lst = []
+        for i in range(n+1):
+            binary = (bin(i)[2:])
+            lst.append(binary.count('1'))
+
+        return lst    
