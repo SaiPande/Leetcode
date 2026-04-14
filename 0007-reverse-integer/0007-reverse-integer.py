e57019ff7cf5d60775cd4b2b87b31b557e3d6cc9
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-04-13 22:24:42 -0400

    Time: 56 ms (7.63%), Space: 19.4 MB (15.54%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+
+        if x == 0:
+            return 0
+
+        stack = []
+        isneg = -1 if x < 0 else 1
+        x1 = abs(x)
+        
+        while x1>0:
+            stack.append(str(x1%10))
+            x1//=10
+
+        reversednum = int(''.join(stack))
+        print(reversednum)
+        
+        if isneg*reversednum <= -2**31 or isneg*reversednum >= (2**31-1):
+            return 0
+        return isneg*reversednum 
