31f16f5d76718eb68e70486c7497ca962ee0f388
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-04-15 14:39:15 -0400

    Time: 0 ms (100%), Space: 17.9 MB (100%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        dict1 = {}
+        lst = []
+        for i in arr:
+            dict1[i] = dict1.get(i,0)+1
+
+        lst.extend(val for val in dict1.values())
+        set1 = set(lst)
+        if len(lst) == len(set1):
+            return True
+        return False    
