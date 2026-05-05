50323c51aeb8d5a8b649bb8a5840fc7e93def278
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-05-05 17:14:52 -0400

    Time: 0 ms (100%), Space: 19.5 MB (14.13%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.py
@@ -1,30 +1,39 @@
 class Solution:
     def minStartValue(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        if len(nums) == 1:
-            if nums[0]<1:
-                return abs(nums[0])+1
-            else:
-                return 0
-        start = 1
-        flag = True
-
-        while flag:
-            pre = [0]*len(nums)
-            
-            if start+nums[0] < 1:
-                start += 1
-                continue
-            else: 
-                pre[0] = start+nums[0]    
-    
-            for i in range(1, len(nums)):          
-                pre[i] = nums[i] + pre[i-1]
+        pre = [0]*len(nums)
+        pre[0] = nums[0]
+        for i in range(1,len(nums)):
+            pre[i] = nums[i]+pre[i-1]   
+        min_sum = min(pre)
         
-                if pre[i] < 1:
-                    break    
-                if i == (len(nums)-1) and pre[i] > 0:
-                    return start     
-            start+=1
-        return start    
+        return max(1, 1-min_sum)
+
+          # if not nums:
+        #     return 0
+        # if len(nums) == 1:
+        #     if nums[0]<1:
+        #         return abs(nums[0])+1
+        #     else:
+        #         return 0
+
+        # start = 1
+        # flag = True
+
+        # while flag:
+        #     pre = [0]*len(nums)
+            
+        #     if start+nums[0] < 1:
+        #         start += 1
+        #         continue
+        #     else: 
+        #         pre[0] = start+nums[0]    
+    
+        #     for i in range(1, len(nums)):          
+        #         pre[i] = nums[i] + pre[i-1]
+        
+        #         if pre[i] < 1:
+        #             break    
+        #         if i == (len(nums)-1) and pre[i] > 0:
+        #             return start     
+        #     start+=1
+        # return start    
