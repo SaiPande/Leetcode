76178c58d830f7f6fe3ffe2c159131a2caba42bf
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-05-15 00:57:02 -0400

    Time: 40 ms (71.12%), Space: 28.7 MB (42.82%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) <= 1:
-            return 0
         sell = 1
         minbuydate = prices[0]
         profit = 0
-        while sell<len(prices):
+        for sell in range(len(prices)):
 
             if prices[sell] < minbuydate:
                 minbuydate = prices[sell]
 
             elif prices[sell]>minbuydate:
-                if profit< prices[sell]-minbuydate:        
-                    profit = prices[sell]-minbuydate
-            
-            sell+=1   
-
+                pro= prices[sell]-minbuydate
+                if profit< pro:        
+                    profit = pro
         return profit
