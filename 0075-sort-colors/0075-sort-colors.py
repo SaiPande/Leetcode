1f885212201409f34ebc2a2b49a45aae9773ab4f
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-06-12 20:09:45 -0400

    Time: 0 ms (100%), Space: 19.2 MB (61.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,14 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            swapped = False
+        count0red = 0
+        count1white = 0
+        count2blue = 0
 
-            for j in range(0,len(nums)-i-1):
+        for i in nums:
+            if i == 0:
+                count0red +=1
+            elif i == 1:
+                count1white +=1
+            else:
+                count2blue +=1  
 
-                if nums[j+1] < nums[j]:
-                    nums[j+1], nums[j] = nums[j], nums[j+1]
-                    swapped = True
-            if swapped == False:
-                break
+        nums[:] = [0]*count0red+[1]*count1white+[2]*count2blue       
+
+
+
+        # for i in range(len(nums)):
+        #     swapped = False
+
+        #     for j in range(0,len(nums)-i-1):
+
+        #         if nums[j+1] < nums[j]:
+        #             nums[j+1], nums[j] = nums[j], nums[j+1]
+        #             swapped = True
+        #     if swapped == False:
+        #         break
 
