b32a8b964a139390b6a1dcfb9e0b5e54d29877de
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-06-13 01:50:16 -0400

    Time: 766 ms (25.82%), Space: 22.3 MB (54.42%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,17 +3,44 @@ class Solution:
         if len(nums)<3:
             return []
         nums.sort()
-        finaloutput = set()
+        finaloutput =[]
 
-        finaloutput = set()
-        for i in range(len(nums)):
-            seen = set()
-            for j in range(i+1,len(nums)):
-                compliment = -(nums[i] + nums[j])
+        for i in range(len(nums)-2):
+            if i>0 and nums[i] == nums[i-1]:
+                continue
+            l = i+1
+            r = len(nums)-1
 
-                if compliment in seen:
-                    finaloutput.add(tuple([nums[i], nums[j], compliment]))
+            while l<r:
+                if nums[l]+nums[r]+nums[i] < 0:
+                    l+=1
+                elif nums[l]+nums[r]+nums[i] > 0:
+                    r-=1
+                else:
+                    finaloutput.append([nums[i], nums[l], nums[r]])
+                    while l<r and nums[l] == nums[l+1]:
+                        l+=1
+                    while l<r and nums[r] == nums[r-1]:
+                        r-=1    
+                    l+=1
+                    r-=1
+        return finaloutput            
 
-                seen.add(nums[j]) 
+        # return [list(t) for t in finaloutput]
+        # if len(nums)<3:
+        #     return []
+        # nums.sort()
+        # finaloutput = set()
 
-        return [list(t) for t in finaloutput]
+        # finaloutput = set()
+        # for i in range(len(nums)):
+        #     seen = set()
+        #     for j in range(i+1,len(nums)):
+        #         compliment = -(nums[i] + nums[j])
+
+        #         if compliment in seen:
+        #             finaloutput.add(tuple([nums[i], nums[j], compliment]))
+
+        #         seen.add(nums[j]) 
+
+        # return [list(t) for t in finaloutput]
