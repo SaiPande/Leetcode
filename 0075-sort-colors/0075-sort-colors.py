fd39705fa1989e96cab1c69778d858ce576cb00a
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-06-12 20:26:31 -0400

    Time: 0 ms (100%), Space: 19.2 MB (61.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,19 +3,41 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count0red = 0
-        count1white = 0
-        count2blue = 0
 
-        for i in nums:
-            if i == 0:
-                count0red +=1
-            elif i == 1:
-                count1white +=1
+        #DUTCH NATIONAL FLAG ALGORITHM
+        # have 3 pointers for each color
+        # left, mid = 0 and right = len(nums)-1
+        # and check the mid, if its 0 swap to left, if it right swap with right, else leave it as is
+
+        l= m = 0
+        r = len(nums)-1
+
+        while m<=r:
+            if nums[m] == 0:
+                nums[m], nums[l] = nums[l], nums[m]
+                l+=1
+                m+=1
+            elif nums[m] == 2:
+                nums[m], nums[r] = nums[r], nums[m]  
+                r-=1
             else:
-                count2blue +=1  
+                m+=1
 
-        nums[:] = [0]*count0red+[1]*count1white+[2]*count2blue       
+
+
+        # count0red = 0
+        # count1white = 0
+        # count2blue = 0
+
+        # for i in nums:
+        #     if i == 0:
+        #         count0red +=1
+        #     elif i == 1:
+        #         count1white +=1
+        #     else:
+        #         count2blue +=1  
+
+        # nums[:] = [0]*count0red+[1]*count1white+[2]*count2blue       
 
 
 
