a2696cc77ae3aab5e944a295175472d0ef4d4918
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-06-18 16:11:30 -0400

    Time: 0 ms (100%), Space: 19.2 MB (96.89%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.py
@@ -1,7 +1,5 @@
 class Solution:
     def angleClock(self, hour: int, minutes: int) -> float:
-        #matrix.sort(key=lambda x: x[0])
-
         hoursangle = hour*30 if hour<12 else 0
         minutesangle = minutes*6 if minutes<60 else 0
         hoursangle+= (30*minutes)/60
