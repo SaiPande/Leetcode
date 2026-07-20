dcf957774473f7aeede6301e824b30dcb5bdbf0d
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-07-20 19:46:27 -0400

    Time: 127 ms (14.05%), Space: 19.6 MB (82.23%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -2,9 +2,10 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         rows, cols = len(grid), len(grid[0])
 
-
+        #### REVISIT THIS! Needed help to solve
         for _ in range(k):
 
+            #Pre-allocating the grid with temp_grid = [[0]*cols for _ in range(rows)] is more optimized for memory and execution time. Initializing exact lengths avoids the memory reallocation overhead that occurs when dynamically expanding empty lists with .append()
             temp_grid = [[0]*cols for _ in range(rows)]
 
             for row in range(rows):
