6061cfaf9c0ee8d05acdc7d79d9d124e4c767305
Sai Pande <56683139+SaiPande@users.noreply.github.com>
2026-09-08 01:36:51 -0400

    Time: 0 ms (100%), Space: 19.3 MB (87.66%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -6,14 +6,33 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        return self.isMirror(root,root)
+    #     return self.isMirror(root,root)
 
-    def isMirror(self, t1, t2):
-        if t1 is None and t2 is None:
-            return True
-        if t1 is None or t2 is None:
-            return False
+    # def isMirror(self, t1, t2):
+    #     if t1 is None and t2 is None:
+    #         return True
+    #     if t1 is None or t2 is None:
+    #         return False
 
-        return (t1.val == t2.val 
-            and self.isMirror(t1.right, t2.left)
-            and self.isMirror(t1.left, t2.right)  )                      
+    #     return (t1.val == t2.val 
+    #         and self.isMirror(t1.right, t2.left)
+    #         and self.isMirror(t1.left, t2.right)  )                      
+
+        q = deque([root, root])
+
+        while q:
+            t1 = q.popleft()
+            t2 = q.popleft()
+            if t1 is None and t2 is None:
+                continue
+            if t1 is None or t2 is None:
+                return False
+
+            if t1.val != t2.val:
+                return False
+            q.append(t1.left)
+            q.append(t2.right)
+            q.append(t1.right)
+            q.append(t2.left)
+        return True    
+
